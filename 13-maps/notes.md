2332e072d84cafac2aa1534aea49ef83ac533594
Freelancer <freelancer@revolution.com>
2020-01-30 16:54:06 +0000

    .

---
+++ 13-maps/notes.md
@@ -0,0 +1,6 @@
+npm i -g parcel-bundler
+quick way of compiling .ts inside html files.
+
+get type definition files from Definitely Typed
+@types/[module-name]
+
---
+++ 13-maps/src/index.ts
@@ -0,0 +1,13 @@
+import { User } from './User';
+// import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+
+// const company = new Company();
+
+const user = new User();
+const customMap = new CustomMap('map');
+
+
+
+customMap.addUserMarker(user)
