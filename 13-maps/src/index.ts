142bcbc86aeb85db49a277047a4165f5cc6b970a
Freelancer <freelancer@revolution.com>
2020-02-06 16:20:18 +0000

    .

--- 13-maps/src/index.ts
+++ 13-maps/src/index.ts
@@ -1,13 +1,15 @@
 import { User } from './User';
-// import { Company } from './Company';
+import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
 
 // const company = new Company();
 
 const user = new User();
+const company = new Company();
 const customMap = new CustomMap('map');
 
 
 
-customMap.addUserMarker(user)
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
